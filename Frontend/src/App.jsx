bee0b652d65f143edbf8c0d03160cd8373ffc93f
SwayamWakodikar <swayam.w06@gmail.com>
2025-09-09 23:48:20 +0530

    Added Transaction to App

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Layout from "./components/Layout";
 import TransactionForm from "./components/TransactionForm";
+import TransactionList from "./components/TransactionList";
 function App() {
   return (
     <>
      <Layout>
       <TransactionForm />
+      <TransactionList />
      </Layout>
     </>
   );
