440d7356eb126b8e152079ba17e5f024028cbb12
SwayamWakodikar <swayam.w06@gmail.com>
2025-09-16 13:49:58 +0530

    Started Backend

--- Backend/server.js
+++ Backend/server.js
@@ -2,3 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
+const port = 5000;
+const app = express();
+app.use(cors());
+app.use(express.json())
+app.get("/", (req, res) => {
+    res.send("Hello from Nether")
+})
+app.post("/something", (req, res) => {
+    console.log(req.body);
+    res.json({ message: "Data received", data: req.body });
+})
+app.listen(port, () => {
+    console.log("Server is Running ");
+})
