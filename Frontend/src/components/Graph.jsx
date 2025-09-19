908361d80918d370d2fd027b8ad5c8ca784ce31c
SwayamWakodikar <swayam.w06@gmail.com>
2025-09-19 15:31:48 +0530

    Started Work on Dynamic Graph

--- Frontend/src/components/Graph.jsx
+++ Frontend/src/components/Graph.jsx
@@ -1,82 +1,89 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import axios from 'axios';
 
-const data = [
-    {
-        name: 'Jan',
-        Expense: 1000,
-        pv: 2400,
-        amt: 2400,
-    },
-    {
-        name: 'Feb',
-        Expense: 3000,
-        pv: 1398,
-        amt: 2210,
-    },
-    {
-        name: 'Mar',
-        Expense: 2000,
-        pv: 9800,
-        amt: 2290,
-    },
-    {
-        name: 'Apr',
-        Expense: 2780,
-        pv: 3908,
-        amt: 2000,
-    },
-    {
-        name: 'May',
-        Expense: 1890,
-        pv: 4800,
-        amt: 2181,
-    },
-    {
-        name: 'Jun',
-        Expense: 2390,
-        pv: 3800,
-        amt: 2500,
-    },
-    {
-        name: 'Jul',
-        Expense: 3490,
-        pv: 4300,
-        amt: 2100,
-    },
-    {
-        name: 'Aug',
-        Expense: 3490,
-        pv: 4300,
-        amt: 2100,
-    },
-    {
-        name: 'Sep',
-        Expense: 3490,
-        pv: 4300,
-        amt: 2100,
-    },
-    {
-        name: 'Oct',
-        Expense: 3490,
-        pv: 4300,
-        amt: 2100,
-    },
-    {
-        name: 'Nov',
-        Expense: 3490,
-        pv: 4300,
-        amt: 2100,
-    },
-    {
-        name: 'Dec',
-        Expense: 3490,
-        pv: 4300,
-        amt: 2100,
-    },
-];
+// const data = [
+//     {
+//         name: 'Jan',
+//         Expense: 1000,
+//         pv: 2400,
+//         amt: 2400,
+//     },
+//     {
+//         name: 'Feb',
+//         Expense: 3000,
+//         pv: 1398,
+//         amt: 2210,
+//     },
+//     {
+//         name: 'Mar',
+//         Expense: 2000,
+//         pv: 9800,
+//         amt: 2290,
+//     },
+//     {
+//         name: 'Apr',
+//         Expense: 2780,
+//         pv: 3908,
+//         amt: 2000,
+//     },
+//     {
+//         name: 'May',
+//         Expense: 1890,
+//         pv: 4800,
+//         amt: 2181,
+//     },
+//     {
+//         name: 'Jun',
+//         Expense: 2390,
+//         pv: 3800,
+//         amt: 2500,
+//     },
+//     {
+//         name: 'Jul',
+//         Expense: 3490,
+//         pv: 4300,
+//         amt: 2100,
+//     },
+//     {
+//         name: 'Aug',
+//         Expense: 3490,
+//         pv: 4300,
+//         amt: 2100,
+//     },
+//     {
+//         name: 'Sep',
+//         Expense: 3490,
+//         pv: 4300,
+//         amt: 2100,
+//     },
+//     {
+//         name: 'Oct',
+//         Expense: 3490,
+//         pv: 4300,
+//         amt: 2100,
+//     },
+//     {
+//         name: 'Nov',
+//         Expense: 3490,
+//         pv: 4300,
+//         amt: 2100,
+//     },
+//     {
+//         name: 'Dec',
+//         Expense: 3490,
+//         pv: 4300,
+//         amt: 2100,
+//     },
+// ];
 function Graph() {
+const [chartData,setChartData]=useState([]);
+const [loading, setLoading] = useState(true);
+const [error, setError] = useState(null);
+const fetchTransactions=async () =>{
+
+}
     return (
         <div className="GraphOuterCard">
             <div className="GraphInnerItems">
